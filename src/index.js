72fdb67b2b89100f307f6cce3989f02323cc61cc
Ezhar <ezharkarim19@gmail.com>
2023-11-04 04:52:42 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import PageNotFound from "./pages/PageNotFound";
+import HomePage from "./pages/HomePage";
+import Collection from './pages/Collection';
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <App />,
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        path: "*",
+        element: <HomePage />
+      },
+      {
+        path: "collections",
+        element: <Collection />
+      },
+     {
+      path: "Car"
+     }
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode>
-    <App />
-  //</React.StrictMode>
+  // <React.StrictMode>
+  <RouterProvider router={router} />
+  // {/* </React.StrictMode> */}
 );
-
 
