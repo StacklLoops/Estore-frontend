fe95d63bf206e2739abf8abe7153535055adc444
Ezhar <ezharkarim19@gmail.com>
2023-11-04 03:35:45 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PageNotFound from './pages/PageNotFound';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <App />,
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        path: "*",
+        element: <HomePage />
+      },
+     
+      // {
+      //   path: "privacy_policy",
+      //   element: (
+      //     <ContentPage paragraph={privacy_policy} heading={"Privacy Policy"} />
+      //   )
+      // },
+      // {
+      //   path: "terms_of_services",
+      //   element: (
+      //     <ContentPage
+      //       paragraph={terms_Of_Services}
+      //       heading={"Terms of services"}
+      //     />
+      //   )
+      // },
+      // {
+      //   path: "community_standard",
+      //   element: (
+      //     <ContentPage
+      //       paragraph={community_standard}
+      //       heading={"Community Standards"}
+      //     />
+      //   )
+      // },
+ 
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  //<React.StrictMode>
+  // <Provider store={store}>
+    <RouterProvider router={router} />
+  // </Provider>
+  // </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
