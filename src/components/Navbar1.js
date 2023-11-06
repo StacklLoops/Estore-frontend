cb1391107e285db1deeb6067f503da21d4ee5977
Ezhar <ezharkarim19@gmail.com>
2023-11-06 15:55:32 +0500

    Add tailwind

--- src/components/Navbar1.js
+++ src/components/Navbar1.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar1() {
   return (
     <div>
-      
+      <h1>Navbar primary</h1>
     </div>
   )
 }
--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar2() {
   return (
     <div>
-        
+        <h1>Secondary Navbar</h1>
     </div>
   )
 }
